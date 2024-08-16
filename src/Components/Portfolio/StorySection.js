b78e716880e8efb5489d9f7acdcda3aa48652f18
prajjwolcodes <shresthaprajjwol4@gmail.com>
2024-08-16 16:03:07 +0545

    Complete portfolio

--- src/Components/Portfolio/StorySection.js
+++ src/Components/Portfolio/StorySection.js
@@ -3,10 +3,15 @@ import prajjwol from '../../Assets/prajjwol.jpg';
 import { AiOutlineClose } from "react-icons/ai";
 import { GrNext, GrPrevious } from "react-icons/gr";
 import { motion } from "framer-motion";
+import logo from '../../Assets/logo-light.png';
+import football from '../../Assets/Highlights/football.jpg';
+import rock from '../../Assets/Highlights/rock.jpg';
+import culture from '../../Assets/Highlights/culture.jpg';
+import travel from '../../Assets/Highlights/travel.jpeg';
+import family from '../../Assets/Highlights/family.jpeg';
 
 const stories = [
-    { id: 1, stories: ["https://marketplace.canva.com/EAFGv9WhSmc/1/0/1600w/canva-blue-and-yellow-minimalist-employee-of-the-month-certificate-jaIc19nYjY4.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpSJhAb2rBt3EhgkbIAYJIbDN1_6vA9XiEw&s", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWJfFCJV-vv9n31dyGa5SQbK7gYflNb-7ClHI55banTVjaxoxMWhrJmO0uqPFe6Da6wv4&usqp=CAU",], alt: "Certificates", text: "Certificates" },
-
+    { id: 1, stories: [rock, football, culture, travel, family], alt: "Extra", text: "Extra" },
 ];
 
 function App() {
@@ -75,9 +80,9 @@ function App() {
                 {stories.map((story) => (
                     <div key={story.id} className="relative flex flex-col gap-2 justify-center items-center">
                         <img
-                            src={story.stories[0]}
+                            src={logo}
                             alt={story.alt}
-                            className="w-[86px] h-[86px] rounded-full cursor-pointer hover:scale-110 transition-transform duration-200"
+                            className="w-[86px] h-[86px] rounded-full border-4 border-gray-200 cursor-pointer hover:scale-110 transition-transform duration-200"
                             onClick={() => handleClick(story)}
                         />
                         <p>{story.text}</p>
@@ -94,14 +99,14 @@ function App() {
                     className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50 `}
                 >
                     <div className="relative bg-gray-900">
-                        <div className='absolute z-50 top-1 px-1 w-full'>
+                        <div className='absolute z-50 top-1 pl-3 py-2 w-full'>
                             <div className="flex gap-3 justify-center items-center">
                                 <div className="w-full h-1 bg-gray-500 rounded overflow-hidden">
-                                    <div ref={lineRef} className="h-full bg-white w-0"></div>
+                                    <div ref={lineRef} className="h-full bg-white"></div>
                                 </div>
 
                                 <button
-                                    className={` text-white transform transition-transform p-2`}
+                                    className={` text-white transform transition-transform pr-2`}
                                     onClick={closeModal}
                                 >
                                     <AiOutlineClose size={20} />
@@ -114,19 +119,22 @@ function App() {
                                     className={`rounded-full w-10 h-10 border border-white transition-colors cursor-pointer`}
                                 />
                                 <h1 className='text-lg text-white font-semibold tracking-normal'>Prajjwol Shrestha</h1>
-                                <span className='text-sm text-gray-400'>2 days ago</span>
+                                <span className='text-sm text-gray-100'>2 days ago</span>
                             </div>
                         </div>
-                        <div
-                            className={`relative `}
-                        >
-                            <img
+                        <div className={`relative`}>
+                            <motion.img
+                                key={storyIndex}
                                 src={selectedStory.stories[storyIndex]}
+                                initial={{ opacity: 0.5, scale: 0.95 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0.5, scale: 0.95 }}
+                                transition={{ duration: 0.5, ease: 'easeInOut' }}
                                 className="max-w-xs sm:max-w-md lg:max-w-lg xl:max-w-xl h-screen rounded-lg"
                             />
                             {storyIndex > 0 && (
                                 <button
-                                    className="absolute left-2 top-1/2 transform -translate-y-1/2 text-black cursor-pointer text-4xl"
+                                    className="absolute -left-16 top-1/2 transform -translate-y-1/2 text-white cursor-pointer text-4xl"
                                     onClick={handlePrevStory}
                                 >
                                     <GrPrevious />
@@ -134,7 +142,7 @@ function App() {
                             )}
                             {storyIndex < selectedStory.stories.length - 1 && (
                                 <button
-                                    className="absolute right-2 top-1/2 transform -translate-y-1/2 text-black cursor-pointer text-4xl"
+                                    className="absolute -right-16 top-1/2 transform -translate-y-1/2 text-white cursor-pointer text-4xl"
                                     onClick={handleNextStory}
                                 >
                                     <GrNext />
